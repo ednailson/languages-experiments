992d9c9432217b59870b0b88629008d04903dbca
ednailsonc <ednailsoncunha@gmail.com>
2019-10-03 09:22:51 -0300

    case

--- elixir/main.exs
+++ elixir/main.exs
@@ -5,8 +5,13 @@ IO.puts :apple
 IO.puts is_binary(:apple)
 
 
-add = fn a,b -> IO.puts a + b end
+add = fn a, b -> IO.puts a + b end
 add.(1, 3)
 
 array = [1, 2, 3]
 IO.puts hd(array)
+
+case {1, 2, 3} do
+  {5} -> IO.puts "It won't match"
+  {1, 2, 3} -> IO.puts "It will match"
+end
