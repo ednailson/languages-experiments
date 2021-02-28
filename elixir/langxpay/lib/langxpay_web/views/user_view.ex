309a0360c6c26a3c95f3c8e25fe37bc7a5cbb8d4
ednailson <evbcjr@gmail.com>
2021-02-27 21:09:45 -0300

    account and migrations

--- elixir/langxpay/lib/langxpay_web/views/user_view.ex
+++ elixir/langxpay/lib/langxpay_web/views/user_view.ex
@@ -1,14 +1,18 @@
 defmodule LangxpayWeb.UserView do
 
-  alias Langxpay.User
+  alias Langxpay.{Account, User}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{user: %User{account: %Account{} = account, id: id, name: name, nickname: nickname}}) do
     %{
       message: "user created",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        nickname: nickname,
+        account: %{
+          id: account.id,
+          balance: account.balance
+        }
       }
     }
   end
